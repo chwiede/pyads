5e76df3a8d40917eace1e6a705d00f03fb83312f
Christoph Wiedemann <chwiede@gmail.com>
2013-11-29 18:02:17 +0100

    setup.py added

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pyads',
+      version='0.1',
+      description='python implementation of ADS protocol',
+      url='http://www.github.com/chwiede/pyads',
+      packages = ['pyads', 'pyads.commands']
+      )
